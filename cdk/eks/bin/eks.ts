300fe9a752745a2f147baf7a393f63f0aaa94e70
Harry <harryryu@amazon.com>
2025-08-21 18:27:42 -0700

    Update eks.ts

--- cdk/eks/bin/eks.ts
+++ cdk/eks/bin/eks.ts
@@ -50,7 +50,7 @@ const knowledgeBaseStack = new KnowledgeBaseStack(app, 'AppSignalsKnowledgeBaseS
 const guardrailStack = new GuardrailStack(app, 'AppSignalsGuardrailStack')
 
 // Add Resource Explorer Stack to enable AWS Resource Explorer
-const resourceExplorerStack = new ResourceExplorerStack(app, 'AppSignalsResourceExplorerStack')
+// const resourceExplorerStack = new ResourceExplorerStack(app, 'AppSignalsResourceExplorerStack')
 
 const eksStack = new EksStack(app, 'AppSignalsEksClusterStack', {
   vpc: networkStack.vpc,
@@ -75,7 +75,7 @@ eksStack.addDependency(transactionSearchStack); // Add dependency on Transaction
 eksStack.addDependency(applicationSignalsStack); // Add dependency on Application Signals Stack
 eksStack.addDependency(knowledgeBaseStack); // Add dependency on Knowledge Base Stack
 eksStack.addDependency(guardrailStack); // Add dependency on Guardrail Stack
-eksStack.addDependency(resourceExplorerStack); // Add dependency on resource explorer
+// eksStack.addDependency(resourceExplorerStack); // Add dependency on resource explorer
 
 const syntheticCanaryStack = new SyntheticCanaryStack(app, 'AppSignalsSyntheticCanaryStack', {
   vpc: networkStack.vpc,
